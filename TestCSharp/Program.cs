04785d910c4d70359cfbcd9c65a9b3ea988eb0f5
HaiHoang <haihm1411@gmail.com>
2022-01-21 21:19:28 +0700

    test 1

--- TestCSharp/Program.cs
+++ TestCSharp/Program.cs
@@ -20,6 +20,9 @@ namespace TestCSharp
             var arrValue = "".Split(" ");
 
             ulong sum = 0, x1;
+
+            // test 1
+
             var x = Math.Sqrt(long.Parse(arrValue[0]));
             if (x * x < long.Parse(arrValue[0]))
             {
